06a55a9a17cf22501cd8b330f0397d1f515ff3eb
Nabil Zhafri <nabil.zhafri.yahaya@gmail.com>
2016-06-23 00:44:18 +0800

    fix(gulp): fix issue with gulp auto build
    - implement fix where it didn't pickup custom app/components styles
    - override font-awesome/AdminLTE bower.json to include right files
    - remove hard coded files in index.html

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -126,6 +126,7 @@ let lintServerTestScripts = lazypipe()
 
 let styles = lazypipe()
     .pipe(plugins.sourcemaps.init)
+    .pipe(plugins.concat, 'app.css')
     .pipe(plugins.cleanCss, {processImportFrom: ['!fonts.googleapis.com']})
     .pipe(plugins.autoprefixer, {browsers: ['last 1 version']})
     .pipe(plugins.sourcemaps.write, '.');
